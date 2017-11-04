d40cba4d470e92407a1fa7edd79beac62c254e3b
Axter <onenyon@gmail.com>
2017-11-04 21:53:14 +0545

    init

--- InComming.user.js
+++ InComming.user.js
@@ -5,7 +5,7 @@
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // @grant         unsafeWindow
 // @run-at        document-start
-// @updateURL     https://raw.githubusercontent.com/onenyon/InComming.js/master/InComming.meta.js
+// @updateURL     https://raw.githubusercontent.com/onenyon/InComming.js/master/InComming.user.js
 // @downloadURL   https://raw.githubusercontent.com/onenyon/InComming.js/master/InComming.user.js
 // ==/UserScript==    
 
