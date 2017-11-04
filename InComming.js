3ed0c2ad88efca2d8dad97b9c72f71d812eee0d4
Axter <onenyon@gmail.com>
2017-11-04 21:22:38 +0545

    Init

---
+++ InComming.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name          InComming.js
+// @version       0.1
+// @description   Loads Next Page On a single one.
+// @include       https://yande.re/post*
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
+// ==/UserScript==    
+
+var $ = unsafeWindow.jQuery;
