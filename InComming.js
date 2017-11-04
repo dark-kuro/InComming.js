7d34bcf869d4715179fa94ae54cad8b49fe06830
Axter <onenyon@gmail.com>
2017-11-04 21:33:30 +0545

    init

--- InComming.js
+++ InComming.js
@@ -3,17 +3,19 @@
 // @version       0.1
 // @description   Loads Next Page On a single one.
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
+// @grant         unsafeWindow
 // @run-at        document-start
 // @updateURL     https://raw.githubusercontent.com/onenyon/InComming.js/master/InComming.js
+// @downloadURL   https://raw.githubusercontent.com/onenyon/InComming.js/master/InComming.js
 // ==/UserScript==    
 
 var $ = unsafeWindow.jQuery;
 
-(function( $ ){
+(function($){
    $.fn.inComming = function(urlRx) {
       alert('hello world '+urlRx);
       return this;
    }; 
-})( jQuery );
+})(jQuery);
 
 $('https://yande.re/post').inComming()
