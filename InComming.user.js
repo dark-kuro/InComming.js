6a7ab464b150954bcfbaa5d7868f69e2d690ca95
Axter <onenyon@gmail.com>
2017-11-04 21:51:04 +0545

    init

--- InComming.user.js
+++ InComming.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          InComming.js
-// @version       0.1
+// @version       0.2
 // @description   Loads Next Page On a single one.
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // @grant         unsafeWindow
