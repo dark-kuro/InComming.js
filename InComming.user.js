4a24cd02038b8e0a681aff12a37affa57372cf13
Axter <onenyon@gmail.com>
2017-11-04 22:03:35 +0545

    init

--- InComming.user.js
+++ InComming.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name          InComming.js
-// @version       0.3
+// @version       0.4
 // @description   Loads Next Page On a single one.
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // @grant         unsafeWindow
 // @run-at        document-start
 // @match         http://*
 // @match         https://*
+// @include       https://yande.re/post*
 // @updateURL     https://raw.githubusercontent.com/onenyon/InComming.js/master/InComming.user.js
 // @downloadURL   https://raw.githubusercontent.com/onenyon/InComming.js/master/InComming.user.js
-// ==/UserScript==    
+// ==/UserScript==
 
 var $ = unsafeWindow.jQuery;
 
