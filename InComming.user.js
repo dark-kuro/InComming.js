3d443eaa6bc656aa23eb813064055a6d4cd99fd4
Axter <onenyon@gmail.com>
2017-11-05 12:07:08 +0545

    New

--- InComming.user.js
+++ InComming.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          InComming.js
-// @version       1.0
+// @version       0.1b
 // @description   Loads Next Page On a single one.
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // @grant         unsafeWindow
@@ -13,15 +13,9 @@
 // ==/UserScript==
 
 
+
 function main($) {
-    $.inComming({
-        link: 'a.next_page',
-        content: 'ul#post-list-posts li'
-    }).load(function(o) {
-        o.find('.javascript-hide').removeClass();
-        o.find('img[width][height]').removeAttr("width height");
-        o.find('li[style*="width"], .inner[style]').removeAttr("style");
-    });
+    $({}).inComming();
 }
 
 (function() {
@@ -36,74 +30,9 @@ function main($) {
     GM_wait();
 
     function letsJQuery($) {
-        $.inComming = function(rule) {
-            var $doc = $url = callback = next = $next = null,
-                $trigger = false;
-            $.extend({
-                link: null,
-                content: 'body'
-            }, rule);
-            if (!rule.link) return;
+        $.fn.inComming = function(callback) {
+            console.log(this)
 
-            var obj = {
-                load: function(_callback) {
-                    if (!callback) callback = _callback;
-                    else if ($doc && callback) callback($doc);
-                }
-            };
-            return (function() {
-                var nextAjax = function(url) {
-                    $.get(url, function(html) {
-                        html = $(html);
-                        next = html.find(rule.link).attr('href');
-                        content = html.find(rule.content);
-                        $doc = $(document);
-
-                        content.appendTo(parent);
-
-                        console.log(next, url);
-                        $next = next;
-
-                        obj.load($doc);
-                        $trigger = false;
-                    });
-                }
-
-                console.log('INJECTING... to ', $(this));
-
-                $(window).scroll(function() {
-                    if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-                        console.log('NEAR BOTTOM');
-
-                        if (!$trigger) {
-                            $trigger = true;
-                            nextAjax($next);
-                            console.log($trigger);
-                        }
-                    }
-                });
-
-                $doc = $(document);
-                if (typeof(rule.link) === 'function') $url = rule.link($doc);
-                else $url = $doc.find(rule.link).attr('href');
-
-                var parent = $doc.find(rule.content).parent();
-
-                $('<div/>', {
-                    text: $url,
-                    style: 'display:inline-block !important'
-                }).appendTo(parent).css({
-                    background: 'red',
-                    width: '100%',
-                    fontSize: 'xx-large'
-                });
-
-                nextAjax($url);
-
-                console.log("init", rule.link, $url);
-
-                return obj;
-            })();
         };
         main($);
     }
