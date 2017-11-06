37fd372fbaa12b41698608e43215685272be8013
Axter <onenyon@gmail.com>
2017-11-06 22:40:19 +0545

    New And Improved

--- InComming.user.js
+++ InComming.user.js
@@ -13,3 +13,80 @@
 // @downloadURL   https://raw.githubusercontent.com/onenyon/InComming.js/master/InComming.user.js
 // ==/UserScript==
 
+function run($) {
+    $({
+        link: 'a.next_page',
+        content: 'ul#post-list-posts li'
+    }).inComming(function(o) {
+        o.find('.javascript-hide').removeClass();
+        // o.find('img[width][height]').removeAttr("width height");
+        // o.find('li[style*="width"], .inner[style]').removeAttr("style");
+    });
+}
+
+
+(function(plugin) {
+    function GM_wait() {
+        if (typeof unsafeWindow.jQuery == 'undefined')
+            window.setTimeout(GM_wait, 100);
+        else
+            unsafeWindow.jQuery(function() {
+                if (typeof plugin === 'function') unsafeWindow.jQuery.fn[plugin.name] = plugin;
+                run(unsafeWindow.jQuery);
+            });
+    }
+    GM_wait();
+})(function inComming(callback) {
+    var $link = this.attr('link'),
+        $select = this.attr('select') || 'body',
+        $callback = function(doc) {
+            if (typeof callback === 'function') callback(doc);
+        };
+    if (!$link) return;
+
+    var Doc = function(elementCss) {
+        this.raw = elementCss;
+        this.html = jQuery(elementCss);
+        this.find = function(css) {
+            return this.html.find(css);
+        };
+        this.href = this.find($link).attr('href');
+    };
+
+    var $doc = new Doc(document),
+        $parent = $doc.find($select).parent(),
+        $trigger = false,
+        $url = (typeof $link === 'function') ? $link($doc) : $doc.href,
+        $next = $url;
+    console.log($doc, $parent, $trigger, $url);
+
+    function nextAjax(url) {
+        $.get(url, function(html) {
+            html = new Doc(html);
+            var next = html.href,
+                content = html.find($select);
+            $doc = new Doc(document);
+
+            console.log(html);
+
+            content.appendTo($parent);
+            $callback($doc);
+            $trigger = false;
+        });
+    };
+    nextAjax($url);
+
+    console.log('INJECTING... to ', $(this));
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+            console.log('NEAR BOTTOM');
+
+            if (!$trigger) {
+                $trigger = true;
+                nextAjax($next);
+                console.log($trigger);
+            }
+        }
+    });
+});
